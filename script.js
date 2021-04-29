0a804b2959da12a207eb1f577b92d754726b3c29
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-04-29 16:04:00 +0200

    Inizio esercizio

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function (event) {
+    const vue = new Vue(
+        {
+            element:'#vueContainer',
+            data: {
+                message:  "TITOLO H1",
+                classes: "big-font"
+
+            },
+            methods: {
+                
+            }
+
+        }
+    );
+})
